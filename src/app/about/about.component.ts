a5d2fca92242e28041ecc21ce85f25ea1d62d7ea
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-04-13 18:06:21 -0400

    add about, contact, and gallery components

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -6,11 +7,27 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+  public ravioliImageDesktopUrl: string = "../../assets/images/ravioli.png";
+  public ravioliImageMobileUrl: string = "../../assets/images/ravioliMobile.png"
 
-  constructor(private translate: TranslateService) { }
+  constructor(
+    private translate: TranslateService, 
+    media: MediaMatcher, 
+    changeDetectorRef: ChangeDetectorRef) { 
+    this.mobileQuery = media.matchMedia('(max-width: 650px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
+
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
 }
